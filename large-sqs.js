845e81f4482017806edba05cd6e506e2682f8089
Diogo <diogolmenezes@gmail.com>
2021-12-17 13:41:37 -0300

    reading messages

--- large-sqs.js
+++ large-sqs.js
@@ -1,4 +1,5 @@
 const AWS = require('aws-sdk');
+const { Consumer } = require('sqs-consumer');
 
 class LargeSqs {
     constructor(connection, collection, queueUrl, sqsOptions, ttl=1296000) {
@@ -12,10 +13,9 @@ class LargeSqs {
             autoCreate: true,
             collection
         }));
-
-        // TODO: TTL para apagar msgs que nao foram processadas depois de X tempo.
     }
 
+    // Store real message at mongoDb and send only the reccord.id to SQS
     async sendMessage(message) {
         // creating mongo record
         const record = await this.model.create({
@@ -37,6 +37,48 @@ class LargeSqs {
             await this.model.deleteOne({ id: record.id });
             throw error;
         }
+    }
+
+    // Retreive the real message on mongoDB
+    async getRecord(message, handleMessage) {
+         if(!message || !this.isJsonString(message.Body) || !JSON.parse(message.Body).id) return null;
+
+         // parse SQS message
+         const json = JSON.parse(message.Body);
+
+         // recover record from mongodb
+         const record = await this.model.findOne({ id: json.id });
+         
+         // send record to application :)
+         await handleMessage(record?.payload);
+
+         // remove record from mongodb
+         await this.model.deleteOne({ id: json.id });
+    }
+
+    // Listen SQS queue and retreive the real message on mongoDB
+    async listen(sqsConsumerOptions, handleMessage, onError, onProcessingError) {
+        const app = Consumer.create({
+            queueUrl: this.queue,
+            ...sqsConsumerOptions,
+            handleMessage: async (message) => {
+                this.getRecord(message, handleMessage);
+            }
+        });
+
+        app.on('error', onError);
+        app.on('processing_error', onProcessingError);
+        app.start();
+    }
+
+    // This string is a Json ?
+    isJsonString(str) {
+        try {
+            JSON.parse(str);
+        } catch (e) {
+            return false;
+        }
+        return true;
     }
 }
 
